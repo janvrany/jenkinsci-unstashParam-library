eef72bb02b4d79150318069ec0cc0d2ff72226dd
Jan Vrany <jan.vrany@fit.cvut.cz>
2017-09-13 16:38:22 +0100

    First shot.

---
+++ vars/unstashParam.groovy
@@ -0,0 +1,30 @@
+import hudson.FilePath
+import hudson.model.ParametersAction
+import hudson.model.FileParameterValue
+import hudson.model.Executor
+
+def call(String name, String fname = null) {
+    def paramsAction = currentBuild.rawBuild.getAction(ParametersAction.class);
+    if (paramsAction != null) {
+        for (param in paramsAction.getParameters()) {
+            if (param.getName().equals(name)) {
+                if (! param instanceof FileParameterValue) {
+                    error "unstashParam: not a file parameter: ${name}"
+                }
+                if (env['NODE_NAME'] == null) {
+                    error "unstashParam: no node in current context"
+                }
+                if (env['WORKSPACE'] == null) {
+                    error "unstashParam: no workspace in current context"
+                }
+                workspace = new FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), env['WORKSPACE'])
+                filename = fname == null ? param.getOriginalFileName() : fname
+                file = workspace.child(filename)
+                file.copyFrom(param.getFile())
+                return filename;
+            }
+        }
+    }
+    error "unstashParam: No file parameter named '${name}'"
+}
+
